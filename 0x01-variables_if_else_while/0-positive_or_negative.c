cc41b79eaf3fe16c92780f33b39699c6130a826e
Nechezzz <ilobigift@gmail.com>
2022-04-08 13:16:57 -0700

    update 0-positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - assign a random number to int everytime
- * it executes, and print it
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -21,4 +21,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	return (0);
 }
+
 
