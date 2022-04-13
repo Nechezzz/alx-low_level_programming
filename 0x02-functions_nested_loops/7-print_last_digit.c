dda98449d27806ccce7d9c74207b12882d12fe8a
Nechezzz <ilobigift@gmail.com>
2022-04-12 17:20:13 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-	last = n % 10;
+	int a;
 
-	if (last < 0)
-		last = last * -1;
-	_putchar(last = '0');
-	return (last);
+	if (n < 0)
+
+		n = -n;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
+
