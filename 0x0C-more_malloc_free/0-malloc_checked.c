2cbff8da0f60138219bebb0e06d2f07cfe59c8e7
Nechezzz <ilobigift@gmail.com>
2022-05-09 08:35:18 -0700

    uu

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,6 @@
 /**
  * *malloc_checked: allocate memory using malloc
  * @b: the number of bytes to allocate
- *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
@@ -14,7 +13,9 @@ void *malloc_checked(unsigned int b)
 	ptr = malloc(b);
 
 	if (ptr == NULL)
+	{
 		exit(98);
+	}
 
 	return (ptr);
 }
